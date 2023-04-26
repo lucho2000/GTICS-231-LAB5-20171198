1fca7450a693f9320ca934cfd6a2095842540c2b
lucho2000 <luis.ramost@pucp.edu.pe>
2023-04-25 20:30:50 -0500

    primero

---
+++ src/main/java/com/example/lab5_20171198/Lab520171198Application.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20171198;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520171198Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520171198Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab5_20171198/Lab520171198ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20171198;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520171198ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
